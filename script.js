87a18aa6059b926d9692462e9500c5bdc544d8a9
REDLanna <101116852+REDLanna@users.noreply.github.com>
2022-04-17 16:30:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+function jeep(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
 
+  function camaro(imgs) {
+    var expandImg = document.getElementById("expandedImg2");
+    var imgText = document.getElementById("imgtext2");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
+  
+  function supra(imgs) {
+    var expandImg = document.getElementById("expandedImg3");
+    var imgText = document.getElementById("imgtext3");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
+ 
+  function velar(imgs) {
+    var expandImg = document.getElementById("expandedImg4");
+    var imgText = document.getElementById("imgtext4");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
+
+  function r35(imgs) {
+    var expandImg = document.getElementById("expandedImg5");
+    var imgText = document.getElementById("imgtext5");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
